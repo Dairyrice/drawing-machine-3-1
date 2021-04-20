4cdb14f0e83c3db2483b7b26b0dab8a2ae5ef1da
Dairyrice <derek.bui@sjsu.edu>
2021-04-20 13:05:14 -0700

    change

--- sketch.js
+++ sketch.js
@@ -1,13 +1,23 @@
 let array = [];
+let noiseOffset = 0.0;
+let strokeWidth = 5;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(220);
 
-  strokeWeight(5);
+
 }
 
 function draw() {
+
+  background(220, 5);
+  strokeWeight(strokeWidth);
+
+  noiseOffset += 0.05;
+  strokeWidth = noise(noiseOffset) * 100;
+
+
 
   if (mouseIsPressed ==true){
     line(mouseX, mouseY, pmouseX, pmouseY);
@@ -20,9 +30,8 @@ function keyTyped(){
 
   if (key === 's'){
     saveCanvas('fileName', 'png' )
-  } else if (key === 'd'){
-
-
+  } else if (key === 'c'){
+    clear();
   }
 
   return false;
